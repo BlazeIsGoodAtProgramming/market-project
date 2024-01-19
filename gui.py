31aa92d775bd8ca25c0020af503c9a918e4c1f4f
django1401 <126799693+django1401@users.noreply.github.com>
2024-01-19 09:28:21 +0330

    change title2

--- gui.py
+++ gui.py
@@ -9,7 +9,7 @@ db = Database('d:/stuff.db')
 win = Tk()
 win.geometry('780x450+440+250')
 win.resizable(0,0)
-win.title(' library')
+win.title('library')
 win.config(bg ='light green')
 
 #================Functions=======================
